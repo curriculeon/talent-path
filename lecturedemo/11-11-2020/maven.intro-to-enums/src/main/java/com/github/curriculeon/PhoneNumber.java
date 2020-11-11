57bdc937e362110ffebba0e6c464652b7a5e3978
git-leon <xleonhunter@gmail.com>
2020-11-11 13:51:04 -0500

    gave demonstration on associative arrays

---
+++ lecturedemo/11-11-2020/maven.intro-to-enums/src/main/java/com/github/curriculeon/PhoneNumber.java
@@ -0,0 +1,20 @@
+package com.github.curriculeon;
+
+public class PhoneNumber {
+    private final String phoneNumber;
+
+    public PhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "PhoneNumber{" +
+                "phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
