6da1f171c4a7eb09cf8bb15c7ab298dad931e8c1
git-leon <xleonhunter@gmail.com>
2020-11-23 19:24:50 -0500

    updated highlowgame and slotsgame

--- lecturedemo/11-23-2020/maven_simple-arcade-afternoon/src/main/java/com/github/curriculeon/arcade/games/cardgames/highlow/HighLowGame.java
+++ lecturedemo/11-23-2020/maven_simple-arcade-afternoon/src/main/java/com/github/curriculeon/arcade/games/cardgames/highlow/HighLowGame.java
@@ -11,32 +11,30 @@ public class HighLowGame extends AbstractGame<HighLowPlayer> {
         IOConsole console = getIOConsole();
         Deck deck = new Deck();
         String userInput = null;
-        do {
-            for (HighLowPlayer player : getPlayerList()) {
-                deck.shuffle();
-                Card visibleCard = deck.pop();
-                int visibleCardValue = visibleCard.getRank().getPrimaryValue();
-                console.println("The current visible card is [ %s ]", visibleCard);
-                userInput = player.play();
-                Card cardToCompareAgainst = deck.pop();
-                int cardToCompareAgainstValue = cardToCompareAgainst.getRank().getPrimaryValue();
-                boolean isCardToCompareAgainstEqual = cardToCompareAgainstValue == visibleCardValue;
-                boolean isCardToCompareAgainstHigher = cardToCompareAgainstValue > visibleCardValue;
-                boolean isCardToCompareAgainstLower = (!isCardToCompareAgainstHigher) && (!isCardToCompareAgainstEqual);
-                boolean hasWonWithEven = "even".equalsIgnoreCase(userInput) && isCardToCompareAgainstEqual;
-                boolean hasWonWithLow = "low".equalsIgnoreCase(userInput) && isCardToCompareAgainstLower;
-                boolean hasWonWithHigh = "high".equalsIgnoreCase(userInput) && isCardToCompareAgainstHigher;
-                boolean hasWon = hasWonWithEven || hasWonWithHigh || hasWonWithLow;
+        for (HighLowPlayer player : getPlayerList()) {
+            deck.shuffle();
+            Card visibleCard = deck.pop();
+            int visibleCardValue = visibleCard.getRank().getPrimaryValue();
+            console.println("The current visible card is [ %s ]", visibleCard);
+            userInput = player.play();
+            Card cardToCompareAgainst = deck.pop();
+            int cardToCompareAgainstValue = cardToCompareAgainst.getRank().getPrimaryValue();
+            boolean isCardToCompareAgainstEqual = cardToCompareAgainstValue == visibleCardValue;
+            boolean isCardToCompareAgainstHigher = cardToCompareAgainstValue > visibleCardValue;
+            boolean isCardToCompareAgainstLower = (!isCardToCompareAgainstHigher) && (!isCardToCompareAgainstEqual);
+            boolean hasWonWithEven = "even".equalsIgnoreCase(userInput) && isCardToCompareAgainstEqual;
+            boolean hasWonWithLow = "low".equalsIgnoreCase(userInput) && isCardToCompareAgainstLower;
+            boolean hasWonWithHigh = "high".equalsIgnoreCase(userInput) && isCardToCompareAgainstHigher;
+            boolean hasWon = hasWonWithEven || hasWonWithHigh || hasWonWithLow;
 
-                if (hasWon) {
-                    getWinnerList().add(player);
-                } else {
-                    console.println("You lose.");
-                }
-                console.println("The next card value was [ %s ]", cardToCompareAgainst);
-                deck.push(visibleCard);
-                deck.push(cardToCompareAgainst);
+            if (hasWon) {
+                getWinnerList().add(player);
+            } else {
+                console.println("You lose.");
             }
-        } while(!"quit".equalsIgnoreCase(userInput));
+            console.println("The next card value was [ %s ]", cardToCompareAgainst);
+            deck.push(visibleCard);
+            deck.push(cardToCompareAgainst);
+        }
     }
 }
--- lecturedemo/11-23-2020/maven_simple-arcade-afternoon/src/main/java/com/github/curriculeon/arcade/games/slots/SlotsGame.java
+++ lecturedemo/11-23-2020/maven_simple-arcade-afternoon/src/main/java/com/github/curriculeon/arcade/games/slots/SlotsGame.java
@@ -2,31 +2,29 @@ package com.github.curriculeon.arcade.games.slots;
 
 import com.github.curriculeon.arcade.games.AbstractGame;
 import com.github.curriculeon.arcade.games.PlayerInterface;
-import com.github.curriculeon.utils.IOConsole;
 
 /**
  * Created by leon on 7/21/2020.
  */
 public class SlotsGame extends AbstractGame<SlotsPlayer> {
+    SlotsReelImage image1;
+    SlotsReelImage image2;
+    SlotsReelImage image3;
+
     @Override
     public void run() {
         for (PlayerInterface player : getPlayerList()) {
-            SlotsReelImage image1 = null;
-            SlotsReelImage image2 = null;
-            SlotsReelImage image3 = null;
             String userInput;
-            do {
-                userInput = player.play();
-                if ("pull-lever".equalsIgnoreCase(userInput)) {
-                    image1 = SlotsReelImage.getRandom();
-                    image2 = SlotsReelImage.getRandom();
-                    image3 = SlotsReelImage.getRandom();
-                } else if ("view-slots".equalsIgnoreCase(userInput)) {
-                    getIOConsole().println("|| %s || %s || %s ||", image1, image2, image3);
-                } else if ("exit".equalsIgnoreCase(userInput)) {
-                    break;
-                }
-            } while (!"exit".equalsIgnoreCase(userInput));
+            userInput = player.play();
+            if ("pull-lever".equalsIgnoreCase(userInput)) {
+                image1 = SlotsReelImage.getRandom();
+                image2 = SlotsReelImage.getRandom();
+                image3 = SlotsReelImage.getRandom();
+            } else if ("view-slots".equalsIgnoreCase(userInput)) {
+                getIOConsole().println("|| %s || %s || %s ||", image1, image2, image3);
+            } else if ("exit".equalsIgnoreCase(userInput)) {
+                break;
+            }
         }
     }
 }
